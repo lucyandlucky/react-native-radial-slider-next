9b945563387526c5810d1550ffd7a78773fb8e3f
lucy.li <lucy.li@quectel.com>
2026-02-02 21:50:10 +0800

    feat: 调整 tailText

--- src/components/RadialSlider/TailText.tsx
+++ src/components/RadialSlider/TailText.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Text as SVGText, G, TSpan } from 'react-native-svg';
 import { useRadialSlider } from './hooks';
 import type { RadialSliderProps, TextTailProps } from './types';
-import { Colors } from '../../theme';
 
 const TailText = (props: TextTailProps) => {
   const { unit, min, max } = props;
@@ -11,15 +10,15 @@ const TailText = (props: TextTailProps) => {
   return (
     <>
       <G transform={`translate(${-20}, ${40})`}>
-        <SVGText fill={Colors.darkCharcoal} fontSize={12}>
-          <TSpan x={startPoint.x} y={startPoint.y}>
+        <SVGText fill={'#6B6F75'} fontSize={12}>
+          <TSpan x={startPoint.x} y={startPoint.y} fontWeight={'bold'}>
             {`${min} ${unit}`}
           </TSpan>
         </SVGText>
       </G>
       <G transform={`translate(${-10}, ${40})`}>
-        <SVGText fill={Colors.darkCharcoal} fontSize={12}>
-          <TSpan x={endPoint.x} y={endPoint.y}>
+        <SVGText fill={'#6B6F75'} fontSize={12}>
+          <TSpan x={endPoint.x} y={endPoint.y} fontWeight={'bold'}>
             {`${max} ${unit}`}
           </TSpan>
         </SVGText>
